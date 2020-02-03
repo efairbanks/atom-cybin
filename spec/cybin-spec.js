c9c6d78c048bdb463d90f85027fc38aab8d3a4f6
Eric Fairbanks <GVMHQ\efairbanks@a-2mhxwevwxi08z.gvmhq.com>
2020-02-03 16:56:50 -0500

    Initial commit.

---
+++ spec/cybin-spec.js
@@ -0,0 +1,73 @@
+'use babel';
+
+import Cybin from '../lib/cybin';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('Cybin', () => {
+  let workspaceElement, activationPromise;
+
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace);
+    activationPromise = atom.packages.activatePackage('cybin');
+  });
+
+  describe('when the cybin:toggle event is triggered', () => {
+    it('hides and shows the modal panel', () => {
+      // Before the activation event the view is not on the DOM, and no panel
+      // has been created
+      expect(workspaceElement.querySelector('.cybin')).not.toExist();
+
+      // This is an activation event, triggering it will cause the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'cybin:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        expect(workspaceElement.querySelector('.cybin')).toExist();
+
+        let cybinElement = workspaceElement.querySelector('.cybin');
+        expect(cybinElement).toExist();
+
+        let cybinPanel = atom.workspace.panelForItem(cybinElement);
+        expect(cybinPanel.isVisible()).toBe(true);
+        atom.commands.dispatch(workspaceElement, 'cybin:toggle');
+        expect(cybinPanel.isVisible()).toBe(false);
+      });
+    });
+
+    it('hides and shows the view', () => {
+      // This test shows you an integration test testing at the view level.
+
+      // Attaching the workspaceElement to the DOM is required to allow the
+      // `toBeVisible()` matchers to work. Anything testing visibility or focus
+      // requires that the workspaceElement is on the DOM. Tests that attach the
+      // workspaceElement to the DOM are generally slower than those off DOM.
+      jasmine.attachToDOM(workspaceElement);
+
+      expect(workspaceElement.querySelector('.cybin')).not.toExist();
+
+      // This is an activation event, triggering it causes the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'cybin:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        // Now we can test for view visibility
+        let cybinElement = workspaceElement.querySelector('.cybin');
+        expect(cybinElement).toBeVisible();
+        atom.commands.dispatch(workspaceElement, 'cybin:toggle');
+        expect(cybinElement).not.toBeVisible();
+      });
+    });
+  });
+});
---
+++ spec/cybin-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CybinView from '../lib/cybin-view';
+
+describe('CybinView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
